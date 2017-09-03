14fed35766741d16a3473110b05f9d895bb73a65
PushkarrajPujari <pushkarraj@pujari.me>
2017-09-04 00:46:09 +0530

    Sanity Check

---
+++ src/Send.java
@@ -0,0 +1,12 @@
+/**
+ * @Author : Pushkarraj Pujari
+ * @Since  : 03/09/2017
+ */
+public class Send {
+    /**
+     * Sanity Check
+     * */
+    public static void main(String[] args) {
+        System.out.println("Worker Queue");
+    }
+}
